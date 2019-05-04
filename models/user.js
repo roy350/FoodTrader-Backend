e18e6c9985f3559daa88929baaff2b64e736e203
Rodrigo Cortes <racortes2@uc.cl>
2019-05-04 18:34:10 -0400

    user CRUD finished

---
+++ models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+async function buildPasswordHash(instance) {
+  if (instance.changed('password')) {
+    const hash = await bcrypt.hash(instance.password, 10);
+    instance.set('password', hash);
+  }
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    'user',
+    {
+      name: DataTypes.STRING,
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      address: DataTypes.STRING,
+      email: DataTypes.STRING,
+      isOrganization: DataTypes.BOOLEAN,
+      isActive: DataTypes.BOOLEAN,
+    },
+    {}
+  );
+
+  user.beforeUpdate(buildPasswordHash);
+  user.beforeCreate(buildPasswordHash);
+
+  user.prototype.checkPassword = function checkPassword(password) {
+    return bcrypt.compare(password, this.password);
+  };
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  return user;
+};
