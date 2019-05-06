4758230de258c98489d93a1fc3ebd20a14b260ce
Rodrigo Cortes <racortes2@uc.cl>
2019-05-06 12:01:10 -0400

    login finished

--- routes.js
+++ routes.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
+
 const KoaRouter = require('koa-router');
+const jwt = require('koa-jwt');
 
 const index = require('./routes/index');
 const user = require('./routes/user');
+const auth = require('./routes/auth');
+const usersPost = require('./routes/userPost');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
+router.use('/auth', auth.routes());
+router.use('/userPost', usersPost.routes());
+
+router.use(jwt({ secret: process.env.WORD_SECRET, key: 'authData' }));
+router.use(async (ctx, next) => {
+  if (ctx.state.authData) {
+    ctx.state.currentUser = await ctx.orm.user.findOne({
+      where: { id: ctx.state.authData.id },
+    });
+  }
+  return next();
+});
+
 router.use('/users', user.routes());
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,91 +1,96 @@
 const KoaRouter = require('koa-router');
+const jwt = require('jsonwebtoken');
 
 const router = new KoaRouter();
 
 router.get('users', '/', async ctx => {
-  try {
-    const users = await ctx.orm.user.findAll({ where: { isActive: true } });
-    ctx.body = users;
-  } catch (validationError) {
-    ctx.throw(500, `${validationError}`);
+  const token = ctx.request.header.authorization.slice(7);
+  const currentUser = jwt.verify(token, process.env.WORD_SECRET);
+  if (currentUser) {
+    try {
+      const users = await ctx.orm.user.findAll({ where: { isActive: true } });
+      ctx.body = users;
+    } catch (validationError) {
+      ctx.throw(500, `${validationError}`);
+    }
   }
 });
 
 router.get('users', '/:id', async ctx => {
-  try {
-    const user = await ctx.orm.user.findOne({
-      where: { id: ctx.params.id, isActive: true },
-    });
-    if (!user) {
-      ctx.status = 404;
-      ctx.message = 'User not found';
-      return ctx.message;
-    }
-    ctx.body = user;
-  } catch (validationError) {
-    ctx.throw(500, `${validationError}`);
-  }
-});
-
-router.post('user.create', '/new', async ctx => {
-  const user = ctx.orm.user.build(ctx.request.body);
-  try {
-    await user.save({
-      fields: [
-        'name',
-        'username',
-        'password',
-        'address',
-        'email',
-        'isOrganization',
-      ],
-    });
-  } catch (validationError) {
-    ctx.throw(500, `${validationError}`);
-  }
-  ctx.body = 'User created correctly';
-  ctx.status = 201;
-});
-
-router.patch('user.update', '/edit/:id', async ctx => {
-  try {
-    const user = await ctx.orm.user.findOne({ where: { id: ctx.params.id } });
-    if (!user) {
-      ctx.status = 404;
-      ctx.message = 'User not found';
-      return ctx.message;
-    }
+  const token = ctx.request.header.authorization.slice(7);
+  const currentUser = jwt.verify(token, process.env.WORD_SECRET);
+  if (currentUser) {
     try {
-      const { name, username, password, address, email } = ctx.request.body;
-      await user.update({ username, name, password, address, email });
+      const user = await ctx.orm.user.findOne({
+        where: { id: ctx.params.id, isActive: true },
+      });
+      if (!user) {
+        ctx.status = 404;
+        ctx.message = 'User not found';
+        return ctx.message;
+      }
+      ctx.body = user;
     } catch (validationError) {
       ctx.throw(500, `${validationError}`);
     }
-    ctx.body = 'User updated correctly';
-    ctx.status = 200;
-  } catch (validationError) {
-    ctx.throw(500, `${validationError}`);
+  }
+});
+
+router.patch('user.update', '/edit/:id', async ctx => {
+  const token = ctx.request.header.authorization.slice(7);
+  const currentUser = jwt.verify(token, process.env.WORD_SECRET);
+  if (currentUser.id === Number(ctx.params.id)) {
+    try {
+      const user = await ctx.orm.user.findOne({ where: { id: ctx.params.id } });
+      if (!user) {
+        ctx.status = 404;
+        ctx.message = 'User not found';
+        return ctx.message;
+      }
+      try {
+        const { name, username, password, address, email } = ctx.request.body;
+        await user.update({ username, name, password, address, email });
+      } catch (validationError) {
+        ctx.throw(500, `${validationError}`);
+      }
+      ctx.body = 'User updated correctly';
+      ctx.status = 200;
+    } catch (validationError) {
+      ctx.throw(500, `${validationError}`);
+    }
+  } else {
+    ctx.status = 401;
+    ctx.message = 'Unauthorized for update user';
+    return ctx.message;
   }
 });
 
 router.del('user.delete', '/:id', async ctx => {
-  try {
-    const user = await ctx.orm.user.findOne({ where: { id: ctx.params.id } });
-    if (!user) {
-      ctx.status = 404;
-      ctx.message = 'User not found';
-      return ctx.message;
-    }
+  const token = ctx.request.header.authorization.slice(7);
+  const currentUser = jwt.verify(token, process.env.WORD_SECRET);
+  if (currentUser.id === Number(ctx.params.id)) {
     try {
-      const isActive = false;
-      await user.update({ isActive });
+      const user = await ctx.orm.user.findOne({ where: { id: ctx.params.id } });
+      if (!user) {
+        ctx.status = 404;
+        ctx.message = 'User not found';
+        return ctx.message;
+      }
+      try {
+        const isActive = false;
+        await user.update({ isActive });
+      } catch (validationError) {
+        ctx.throw(500, `${validationError}`);
+      }
+      ctx.body = 'User deleted correctly';
+      ctx.status = 200;
     } catch (validationError) {
       ctx.throw(500, `${validationError}`);
     }
-    ctx.body = 'User deleted correctly';
-    ctx.status = 200;
-  } catch (validationError) {
-    ctx.throw(500, `${validationError}`);
+  } else {
+    ctx.status = 401;
+    ctx.message = 'Unauthorized for delete user';
+    return ctx.message;
   }
 });
 
