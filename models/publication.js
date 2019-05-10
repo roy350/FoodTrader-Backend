3aa23a92120bdaa5126a4e764a801f33e49253b7
Rodrigo Cortes <racortes2@uc.cl>
2019-05-09 22:59:59 -0400

    add image and error handling

--- models/publication.js
+++ models/publication.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
       content: DataTypes.STRING,
       isActive: DataTypes.BOOLEAN,
       place: DataTypes.STRING,
+      image: DataTypes.STRING,
     },
     {}
   );
