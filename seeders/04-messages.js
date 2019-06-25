611daa1338e31844c857973bc9520eb82a3bfa92
Rodrigo Cortes <racortes2@uc.cl>
2019-06-25 17:38:43 -0400

    seeds for chats and messages

---
+++ seeders/04-messages.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'chats',
+      [
+        {
+          content: 'Hola Sebi',
+          userId: 4,
+          chatId: 1,
+          createdAt: '2019-04-14 19:11:36.847+00',
+          updatedAt: '2019-04-14 19:11:36.847+00',
+        },
+        {
+          content: 'Hola Javi',
+          userId: 3,
+          chatId: 1,
+          createdAt: '2019-04-14 19:11:36.847+00',
+          updatedAt: '2019-04-14 19:11:36.847+00',
+        },
+        {
+          content: 'Hola Jorge',
+          userId: 2,
+          chatId: 2,
+          createdAt: '2019-04-14 19:11:36.847+00',
+          updatedAt: '2019-04-14 19:11:36.847+00',
+        },
+        {
+          content: 'Hola Roy',
+          userId: 1,
+          chatId: 2,
+          createdAt: '2019-04-14 19:11:36.847+00',
+          updatedAt: '2019-04-14 19:11:36.847+00',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async queryInterface => {
+    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.sequelize.query(
+      'alter sequence users_id_seq restart with 1;'
+    );
+  },
+};
