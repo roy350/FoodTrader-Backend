8664ae4285ca696710cbf2991a5ebdac9fed5aa0
Rodrigo Cortes <racortes2@uc.cl>
2019-06-25 17:48:15 -0400

    change error

--- seeders/04-messages.js
+++ seeders/04-messages.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
-      'chats',
+      'messages',
       [
         {
           content: 'Hola Sebi',
